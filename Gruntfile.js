ec5875839b70ff897948d46cfec0521345a091a7
Patrick Coffey <patrickcoffey48@gmail.com>
2013-04-16 23:17:36 -0500

    Grunt file that zips up output files, modifying package to reflect changed packages, adding gitignore, introducing empty stylesheet

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,66 +2,34 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     recess: {
-      main: {
-        options: {
-          compile: true,
-          compress: false,
-        },
-        files: {
-          'assets/css/style.css': [
-            'assets/css/bootstrap/bootstrap.less',
-            'assets/css/bootstrap/responsive.less',
-            'assets/css/icons.css',
-            'assets/css/app.less'
-          ]
-        }
+      options: {
+        compile: false
+      },
+      files: {
+        'assets/css/style.css': [
+          'assets/css/app.less'
+        ]
       }
     },
-    inlinecss: {
-      main: {
-        files: {
-          'email.html' : 'email.inline.html',
-        }
-      }
-    },
-    comress: {
-      main: {
-        options: {
-          archive: 'final.zip',
-          mode: 'gzip',
-        },
-        files: [
-          {src: ['email.inline.html'], dest: 'email.html', filter: 'isFile'},
-          {src: ['assets/img/**'], dest: 'assets/img/'},
-        ],
-      }
+    zip: {
+      'output/final.zip': [
+        'email.html',
+        'assets/css/style.css',
+        'assets/img/',
+      ]
     },
     watch: {
       less: {
-        files: [
-          'assets/css/app.less',
-          'assets/css/bootstrap/bootstrap.less'
-        ],
+        files: ['assets/css/app.less'],
         tasks: ['recess']
       }
-    }
+    },
   });
 
-  grunt.loadTasks('tasks');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-recess');
-  grunt.loadNpmTasks('grunt-inline-css');
-  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-zip');
 
-  grunt.registerTask('default', [
-    'recess',
-    'inlinecss',
-    'compress',
-  ]);
+  grunt.registerTask('default', ['recess', 'zip']);
 
-  grunt.registerTask('final', [
-    'recess',
-    'inline',
-    'compress',
-  ]);
 };
