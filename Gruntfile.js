0b44ebb05904efad38b74a8d88daccefb97975db
Patrick Coffey <patrickcoffey48@gmail.com>
2013-04-17 00:18:12 -0500

    adding bootstrap, fixing several issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,39 @@
 'use strict';
 module.exports = function(grunt) {
   grunt.initConfig({
-    recess: {
-      options: {
-        compile: false
-      },
-      files: {
-        'assets/css/style.css': [
-          'assets/css/app.less'
-        ]
+    less: {
+      default: {
+        files: {
+          'assets/css/style.css': [
+            'assets/css/bootstrap/bootstrap.less',
+            'assets/css/bootstrap/responsive.less',
+            'assets/css/app.less'
+          ],
+        }
       }
     },
     zip: {
-      'output/final.zip': [
-        'email.html',
-        'assets/css/style.css',
-        'assets/img/',
-      ]
+      default: {
+        src: [
+          'email.html',
+          'assets/img/',
+          'assets/css/style.css'
+        ],
+        dest: 'output/final.zip'
+      }
     },
     watch: {
       less: {
         files: ['assets/css/app.less'],
-        tasks: ['recess']
+        tasks: ['less']
       }
     },
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-recess');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-zip');
 
-  grunt.registerTask('default', ['recess', 'zip']);
+  grunt.registerTask('default', ['less', 'zip']);
 
 };
